9ce81d74d1756424a45c9849adaaa7741d6a4b78
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2024-08-18 21:15:26 +0100

    added debounce

---
+++ FindMoreAcademy/js/2-performance/2.debounce.js
@@ -0,0 +1,3 @@
+//Debounce
+//Técnica para filtrar valores que mudam rapidamente e emitir o valor mais recente, apenas após um período definido em que não há emissão ou execução.
+//Só executa após não haver interação durante deteerminado tempo.
