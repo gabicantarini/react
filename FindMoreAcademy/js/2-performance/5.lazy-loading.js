4338f89204e1c63066b0f9943feccf2dab381a7d
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2024-08-20 14:47:17 +0100

    add style

--- FindMoreAcademy/js/2-performance/5.lazy-loading.js
+++ FindMoreAcademy/js/2-performance/5.lazy-loading.js
@@ -1,5 +1,34 @@
-[...Array.from(Array(arrayLength).keys())].forEach(() =>{
+[...Array.from(Array(100).keys())].forEach((item) =>{
     const image = `
-        <div>
+        <div class="img-wrapper">
+            <img
+                class="lazy"
+                src="https://placehold.co/300X300?text=Coming+Soon"
+                data-src="https://picsum.photos/id/${item}/500"
+                alt="Image Alt-${item}"
+            >
+        </div>
     `
+    document.querySelector(".img-list").innerHTML += image
+})
+
+const lazyImages = document.querySelectorAll(".lazy")
+
+const handleIntersection = (entries, observer) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            const img = entry.target
+            const src = img.getAttribute("data-src")
+            // Trocamos o placeholder pelo data source
+            img.src = src
+            // Parar de observer a image
+            observer.unobserve(img)
+        }
+    })
+}
+
+const observerLazyLoading = new IntersectionObserver(handleIntersection)
+
+lazyImages.forEach((image) => {
+    observerLazyLoading.observe(image)
 })
