741cb3dfe2870f304d2d606acf10d31817253d41
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2024-09-03 15:44:54 +0100

    add article logic

--- BlogApp/src/App.tsx
+++ BlogApp/src/App.tsx
@@ -1,17 +1,35 @@
-import './App.css'
+import './App.css';
+import { NavBar } from './NavBar';
+import { HomePage } from './pages/HomePage';
+import { AboutPage } from './pages/AboutPage';
+import { ArticlesPages } from './pages/ArticlesPages';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ArticlesListPage } from './pages/ArticlesListPage';
+import { NotFoundPage } from './pages/NotFoundPage';
+
 
 function App() {
   
   return (
-    <>
+    <BrowserRouter>
       <div className="App">
-        <h1>Welcome to my Blog</h1>
+        <NavBar />
           <div className="page-body">
-            This is my blog.
+            <Routes>
+              <Route path="/" element={<HomePage />}/>
+              <Route path="/about" element={<AboutPage />}/>
+              <Route path="/articles/:articleId" element={<ArticlesPages />}/>
+              <Route path="/articles" element={<ArticlesListPage />}/>
+              <Route path="/" element={<NotFoundPage />}/>          
+            </Routes>         
           </div>
       </div>
-    </>
+    </BrowserRouter>
   )
 }
 
 export default App
+
+
+//install react router to create routes => npm install react-router-dom
